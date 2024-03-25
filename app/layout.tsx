03f8673cfccf959308c214fe9f08c77bdfd4a80f
mayank-dev07 <devmayank.hts@gmail.com>
2024-03-25 14:58:55 +0530

    completed

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,4 @@
-import { Poppins } from "next/font/google";
 import "./globals.css";
-
-export const poppins = Poppins({
-  weight: ["400", "600", "700"],
-  subsets: ["latin"],
-  style: ["normal"],
-  preload: true,
-});
 
 export default function RootLayout({
   children,
@@ -15,7 +7,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${poppins.className} min-h-screen`}>{children}</body>
+      <body className={` min-h-screen`}>{children}</body>
     </html>
   );
 }
